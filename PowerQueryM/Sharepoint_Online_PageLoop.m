c6cb8787a641c733807cf807c55995e85bec0020
Altheran88 <38200467+Altheran88@users.noreply.github.com>
2020-09-17 21:35:31 -0400

    Update Sharepoint_Online_PageLoop.m
    Added comments in the code to explain requirements and limitations.

--- PowerQueryM/Sharepoint_Online_PageLoop.m
+++ PowerQueryM/Sharepoint_Online_PageLoop.m
@@ -27,3 +27,12 @@ let SHPQueries =
 
 in 
 SHPQueries
+
+// This doesn't run in the cloud refresh as it is considered a "dynamic" Web.Contents()
+// I pass the Parameters from the first step of the query being text input to let the user choose the site, listname,
+// pagelimit and the query that is executed on the sharepoint site. Be wary that filtering non-indexed columns will fail if your total number of rows exeeds 5000
+// or the limit configured by your adminitrator wichever is lower.
+        
+// Need to try with static values fixed in the function to see if PowerBI online still sees this quesry function as a "Dynamic Web.Contents".
+// Like writing the URL as :
+// url = (https://companyname.sharepoint.com/sites/sitename&"_api/web/lists/getbytitle('ListName')/items?$top=5000),
